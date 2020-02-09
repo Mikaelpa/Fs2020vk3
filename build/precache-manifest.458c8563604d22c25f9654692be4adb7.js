8f986ec5b5966406023997229b668a899763cf96
Mikaelpa <palmroosmikael@gmail.com>
2020-02-09 23:41:06 +0200

    test3

---
+++ build/precache-manifest.458c8563604d22c25f9654692be4adb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c17555f2effca0bb89eb",
+    "url": "/static/js/main.c17555f2.chunk.js"
+  },
+  {
+    "revision": "9224a121380f31e19ae5",
+    "url": "/static/js/1.9224a121.chunk.js"
+  },
+  {
+    "revision": "c17555f2effca0bb89eb",
+    "url": "/static/css/main.264fa144.chunk.css"
+  },
+  {
+    "revision": "3b44c3460e3a128f26ada96a1b8810cf",
+    "url": "/index.html"
+  }
+];
