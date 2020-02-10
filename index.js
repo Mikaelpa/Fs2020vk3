0de7a81dba78f1d07b4139cce920b9c399b989db
Mikaelpa <palmroosmikael@gmail.com>
2020-02-10 03:25:53 +0200

    pls!

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
-
-//app.use(express.static('build'))
+require('dotenv').config();
+app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
