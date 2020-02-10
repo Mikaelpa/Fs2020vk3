cce10068454dbdd18847db9754022757213a6260
Mikaelpa <palmroosmikael@gmail.com>
2020-02-10 03:49:13 +0200

    test

--- models/person.js
+++ models/person.js
@@ -1,28 +1,31 @@
+
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true })
-  .then(res => {
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
+    console.log('error connecting to MongoDB:', error.message)
   })
 
-  const personSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
     name: {type: String, unique: true, minlength: 3},
     number: {type: String, minlength: 8},
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returned) => {
+        returned.id = returned._id.toString()
+        delete returned._id
+        delete returned.__v
+    }
 })
+
 personSchema.plugin(uniqueValidator)
+
 module.exports = mongoose.model('Person', personSchema)
